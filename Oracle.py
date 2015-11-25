d53eef80c5dbfc56b0b8de5be678e401b3b275ed
Akash Bharadwaj <akash.bharadwaj92@yahoo.com>
2015-11-24 20:47:04 -0500

    Working but below baseline

--- Oracle.py
+++ Oracle.py
@@ -1,8 +1,43 @@
 import sys
 from Transition import Transition
+from helper_functions import Helper as p
 
 class Oracle:
-	def getTransition(self, stack, buff, leftmostChildren, rightmostChildren, arcs, labeled):
-    """This function should return a Transition object representing the correct action to
-    to take according to the oracle."""
-		assert False, 'Please implement this function!'
+
+    def is_removable(self, word, arcs, leftmost_child, rightmost_child):
+        if ( (leftmost_child != p.INFINITY and not arcs.get(leftmost_child, False)) or (rightmost_child != -1 and not arcs.get(rightmost_child, False)) ):
+            return(False)
+        if ( p.is_root(word) ):
+            return(False)
+        return True
+
+    def getTransition(self, stack, buff, leftmostChildren, rightmostChildren, arcs, labeled):
+        """This function should return a Transition object representing the correct action to
+        to take according to the oracle."""
+        if len(stack) > 1:
+            top = stack[-1]
+            pre_top = stack[-2]
+            rmc_top = rightmostChildren.get(p.get_id(top), -1)
+            rmc_pre_top = rightmostChildren.get(p.get_id(pre_top), -1)
+            lmc_top = leftmostChildren.get(p.get_id(top), p.INFINITY)
+            lmc_pre_top = leftmostChildren.get(p.get_id(pre_top), p.INFINITY)
+            if ( p.get_head(pre_top) == p.get_id(top) and self.is_removable(pre_top, arcs, lmc_pre_top, rmc_pre_top) ):
+                if labeled:
+                    return(Transition(Transition.LeftArc, p.get_deprel(pre_top)))
+                else:
+                    return(Transition(Transition.LeftArc, None))
+            elif ( p.get_head(top) == p.get_id(pre_top) and self.is_removable(top, arcs, lmc_top, rmc_top) ):
+                if labeled:
+                    return(Transition(Transition.RightArc, p.get_deprel(top)))
+                else:
+                    return(Transition(Transition.RightArc, None))
+            else:
+                return(Transition(Transition.Shift, None))
+        else:
+            if len(buff) >= 1:
+                return(Transition(Transition.Shift, None))
+            else:
+                return(None)
+
+
+        #assert False, 'Please implement this function!'
---
+++ helper_functions.py
@@ -0,0 +1,77 @@
+class Helper:
+    INFINITY = 100000000
+
+
+    ####################################################################################################################
+    ##################################### H E L P E R    F U N C T I O N S #############################################
+    ####################################################################################################################
+
+
+
+    @staticmethod
+    def is_root(word):
+        if Helper.get_id(word) == 0:
+            return(True)
+        else:
+            return(False)
+
+
+    @staticmethod
+    def get_id(word):
+        return(int(word[0].strip(' \t\r\n')))
+
+    @staticmethod
+    def get_word(word):
+        return(word[1].strip(' \t\r\n'))
+
+    @staticmethod
+    def get_stem(word):
+        return(word[2].strip(' \t\r\n'))
+
+    @staticmethod
+    def get_cpostag(word):
+        return(word[3].strip(' \t\r\n'))
+
+    @staticmethod
+    def get_postag(word):
+        return(word[4].strip(' \t\r\n'))
+
+    @staticmethod
+    def get_feats(word):
+        return(word[5].strip(' \t\r\n'))
+
+    @staticmethod
+    def get_head(word):
+        return(int(word[6].strip(' \t\r\n')))
+
+    @staticmethod
+    def get_deprel(word):
+        return(word[7].strip(' \t\r\n'))
+
+    @staticmethod
+    def get_phead(word):
+        return(int(word[8].strip(' \t\r\n')))
+
+    @staticmethod
+    def get_pdeprel(word):
+        return(word[9].strip(' \t\r\n'))
+
+
+    @staticmethod
+    def get_leftmost_children(sentence):
+        leftmost_children = {}
+        for word in sentence:
+            lc_candidate = min(leftmost_children.get(Helper.get_head(word), Helper.INFINITY), Helper.get_id(word))
+            if lc_candidate < Helper.get_head(word):
+                leftmost_children[Helper.get_head(word)] = lc_candidate
+        return leftmost_children
+
+    @staticmethod
+    def get_rightmost_children(sentence):
+        rightmost_children = {}
+        for word in sentence:
+            rc_candidate = max(rightmost_children.get(Helper.get_head(word), -1), Helper.get_id(word))
+            if rc_candidate > Helper.get_head(word):
+                rightmost_children[Helper.get_head(word)] = rc_candidate
+        return(rightmost_children)
+
